d213a9578475c4c99515a266740f61b871e41c70
SriVarshan733 <srivarshan.it20@bitsathy.ac.in>
2023-08-22 11:22:02 +0530

    database rds aws

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'bidding-test_db');
+$conn = new mysqli('database-1.cwa1v3hdvy5b.us-east-1.rds.amazonaws.com', 'admin', 'admin123', 'bidding');
 
 if ($conn->connect_error) {
     die("Could not connect to MySQL: " . $conn->connect_error);
